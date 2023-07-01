844a52caaba1c21af18ed13ea8a7fb09e4faa055
KarimB1980 <96720369+KarimB1980@users.noreply.github.com>
2023-07-01 10:11:12 +0200

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 import { useCreateEmployeeMutation } from '../redux/apiSlice'
 import styles from './style/Home.module.css'
 import Select from 'react-select'
+
 import { Modal } from '@k90891695/modalnpm'
 import { useGetAllStatesQuery } from "../redux/apiSlice"
 import { useGetAllDepartmentsQuery } from "../redux/apiSlice"
 
 function Home() {
-
   let [isOpen, setIsOpen] = useState(false)
   let [isOpenError, setIsOpenError] = useState(false)
 
@@ -18,9 +18,9 @@ function Home() {
   const [user, setUser] = useState([])
 
   const handleSubmit = (e) => {
-      e.preventDefault()
+    e.preventDefault()
 
-    if(user.firstName!==undefined & user.lastName!==undefined & ClickBirthDateCalendar!=='Invalid Date' & ClickStartDateCalendar!=='Invalid Date' & Department!==undefined & user.street!==undefined & user.city!==undefined & State!==undefined & user.zipCode!==undefined) {
+    if (user.firstName !== undefined & user.lastName !== undefined & ClickBirthDateCalendar !== 'Invalid Date' & ClickStartDateCalendar !== 'Invalid Date' & Department !== undefined & user.street !== undefined & user.city !== undefined & State !== undefined & user.zipCode !== undefined) {
 
       console.log('submit')
 
@@ -47,31 +47,24 @@ function Home() {
       setIsOpenError(true)
     }
   }
-  
+
   const onChange = (e) => {
     setUser({
       ...user,
       [e.target.name]: e.target.value
     })
+    console.log(e.target.name)
   }
 
-  const [dateOfBirth, setDateOfBirth] = useState('')
-  const onNativeBirthdateChange = e => {
-    setDateOfBirth(e.target.value);
-  };
-
-  const [startDate, setStartDate] = useState('')
-  const onNativeStartdateChange = e => {
-    setStartDate(e.target.value);
-  };
-
-  const ClickBirthDateCalendar = new Date(dateOfBirth).toLocaleDateString()
-  const ClickStartDateCalendar = new Date(startDate).toLocaleDateString()
+  const ClickBirthDateCalendar = new Date(user.dateOfBirth).toLocaleDateString()
+  const ClickStartDateCalendar = new Date(user.startDate).toLocaleDateString()
 
   const [addNewEmployee] = useCreateEmployeeMutation()
 
   const State = selectState.label
   const Department = selectDepartment.label
+
+  console.log(State)
 
   const { data } = useGetAllStatesQuery()
   const [statesData, setStatesData] = useState([])
@@ -85,16 +78,8 @@ function Home() {
     setDepartmentsData(currentData)
   }, [currentData])
 
-  console.log(user.firstName)
-  console.log(user.lastName)
-  console.log(ClickBirthDateCalendar)
-  console.log(ClickStartDateCalendar)
-  console.log(Department)
-  console.log(State)
-  console.log(user.street)
-  console.log(user.city)
-  console.log(State)
-  console.log(user.zipCode)
+  console.log(statesData)
+
 
   return (
     <div>
@@ -107,58 +92,59 @@ function Home() {
         <form id="create-employee" name="createemployee" onSubmit={handleSubmit}>
 
           <label htmlFor="firstName">First Name</label>
-          <input 
+          <input
             name='firstName'
-            type="text" 
-            id="firstName" 
-            value={user.firstName || ""} 
+            type="text"
+            id="firstName"
+            value={user.firstName || ""}
             onChange={onChange}
           />
 
           <label htmlFor="lastName">Last Name</label>
-          <input 
+          <input
             name='lastName'
-            type="text" 
-            id="lastName" 
-            value={user.lastName || ""} 
+            type="text"
+            id="lastName"
+            value={user.lastName || ""}
             onChange={onChange}
           />
 
           <label>Date of birth </label>
-          <input 
-            type="date" 
-            name="dateOfBirth" 
-            value={dateOfBirth} 
-            onChange={onNativeBirthdateChange} 
+          <input
+            type="date"
+            name="dateOfBirth"
+            value={user.dateOfBirth || ''}
+            onChange={onChange}
           />
 
 
           <label>Start date</label>
-          <input 
-            type="date" 
-            name="startDate" 
-            value={startDate} 
-            onChange={onNativeStartdateChange} 
+          <input
+            type="date"
+            name="startDate"
+            value={user.startDate || ''}
+            onChange={onChange}
+
           />
 
           <fieldset className="address">
             <legend>Address</legend>
 
             <label htmlFor="street">Street</label>
-            <input 
+            <input
               name='street'
-              id="street" 
-              type="text" 
-              value={user.street || ""} 
+              id="street"
+              type="text"
+              value={user.street || ""}
               onChange={onChange}
             />
 
             <label htmlFor="city">City</label>
-            <input 
+            <input
               name='city'
-              id="city" 
-              type="text" 
-              value={user.city || ""} 
+              id="city"
+              type="text"
+              value={user.city || ""}
               onChange={onChange}
             />
 
@@ -167,15 +153,16 @@ function Home() {
               className='data-state'
               classNamePrefix='selectState'
               options={statesData}
+              value={selectState}
               onChange={setSelectState}
             />
 
             <label htmlFor="zipCode">Zip Code</label>
-            <input 
+            <input
               name='zipCode'
-              id="zipCode" 
-              type="number" 
-              value={user.zipCode || ""} 
+              id="zipCode"
+              type="number"
+              value={user.zipCode || ""}
               onChange={onChange}
             />
           </fieldset>
@@ -185,11 +172,15 @@ function Home() {
             className='data-department'
             classNamePrefix='selectDepartment'
             options={departmentsData}
+            value={selectDepartment}
             onChange={setSelectDepartment}
           />
 
           <button className={styles.primaryBtn} id='save' type="submit">Save</button>
-          {isOpen && <Modal setIsOpen={() => {setIsOpen(); window.location.reload(false)}} color={'#d0fefd'} information={'Information'} commentary={'Employee Created !'} action={'Close'} />}
+          {/* {isOpen && <Modal setIsOpen={() => {setIsOpen(); setUser([]); setSelectState(''); setSelectDepartment(''); setDateOfBirth(''); setStartDate('')}} color={'#d0fefd'} information={'Information'} commentary={'Employee Created !'} action={'Close'} />} */}
+          {isOpen && <Modal setIsOpen={() => { setIsOpen(); setUser([]); }} color={'#d0fefd'} information={'Information'} commentary={'Employee Created !'} action={'Close'} />}
+
+
           {isOpenError && <Modal setIsOpen={setIsOpenError} color={'#fecccc'} information={'Information'} commentary={'Employee Not Created ! Please complete all fields.'} action={'Close'} />}
 
         </form>
